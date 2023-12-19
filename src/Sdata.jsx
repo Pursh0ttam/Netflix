e3f03d1e25cd32f4e90409948761c4be75022a0e
Purshottam <kumarpurshottam474@gmail.com>
2023-12-20 00:33:11 +0530

    first commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,91 @@
+// const Sdata = [
+//     {
+//         imgsrc:'https://flxt.tmsimg.com/assets/p10182728_b_v13_bd.jpg',
+//         title:'A Netflix Orignal series',
+//         sname:'Peaky Blinders',
+//         link:'https://www.netflix.com/in/title/80002479',
+//     },
+
+//     {
+//         imgsrc:'https://m.media-amazon.com/images/M/MV5BMWVhNTgwMWEtN2U4Yi00NDhhLWIxOWQtNDc2Y2Y1MTFmM2U1XkEyXkFqcGdeQXVyMTEwMTcyMzkz._V1_FMjpg_UX1000_.jpg',
+//         title:'A Netflix Orignal series',
+//         sname:'My first two love',
+//         link:'https://www.netflix.com/in/title/81026700'
+      
+//     },
+// ];
+
+// export default Sdata;
+
+const Sdata=[
+    {
+        imgsrc:'https://flxt.tmsimg.com/assets/p10182728_b_v13_bd.jpg',
+        title:'A Netflix Orignal series',
+        sname:'Peaky Blinders',
+        link:'https://www.netflix.com/in/title/80002479',
+        // link:'https://vegamovies.dad/download-peaky-blinders-season-1-5-in-english-480p-720p/',
+    },
+    {
+        imgsrc:'https://m.media-amazon.com/images/M/MV5BN2IzYzBiOTQtNGZmMi00NDI5LTgxMzMtN2EzZjA1NjhlOGMxXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_FMjpg_UX1000_.jpg',
+        title:'A Netflix Orignal series',
+        sname:'Games of Thrones',
+        link:'https://www.imdb.com/title/tt0944947/?ref_=tt_mv_close',
+    },
+    {
+        imgsrc:'https://i0.wp.com/thsindex.org/wp-content/uploads/2023/04/Money-Heist-was-first-released-on-Antena-3-in-Spain-in-2017-Netflix.jpg?fit=1920%2C2560&ssl=1',
+        title:'A Netflix Orignal series',
+        sname:'Money Hiest',
+        link:'https://www.netflix.com/in/title/80192098',
+    },
+    {
+        imgsrc:'https://www.themoviedb.org/t/p/w500/h6Z2oogE4mJk2uffdtIlLhb0EHx.jpg',
+        title:'A Netflix Orignal series',
+        sname:'Lupin',
+        link:'https://www.netflix.com/in/title/80994082',
+    },
+    
+    {
+        imgsrc:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOmGvFKs5haQUFJ3MTgVPIivUoX43vAa9lhi8h25i965Tsb2CJMhj7kBd-55YEQ85l2K8&usqp=CAU',
+        title:'A Netflix Orignal series',
+        sname:'Loki ',
+        link:'https://streamnoads.com/v/1Jm1GRzjKvh6Dm/Loki.S02.E01.720p.WEB-DL.Hindi.5.1-English.5.1.ESub.x264-HDHub4u.Tv.mkv',
+    },
+            
+    {
+        imgsrc:'https://wallpapercave.com/wp/wp4056410.jpg',
+        title:'A Netflix Orignal series',
+        sname:'DARK',
+        link:'https://www.netflix.com/in/title/80990668?source=35',
+    },
+    
+    {
+        imgsrc:'https://e1.pxfuel.com/desktop-wallpaper/3/598/desktop-wallpaper-stranger-things-s1-s2-s3-1920x1080-stranger-things-1.jpg',
+         title:'A Netflix Orignal series',
+        sname:'stranger things',
+        link:'https://www.netflix.com/in/title/80057281',
+    },
+    {
+        imgsrc:'https://ntvb.tmsimg.com/assets/p10700229_b_h10_aa.jpg?w=1280&h=720',
+        title:'A Netflix Orignal series',
+        sname:'The Vampire Diaries',
+        link:'https://www.netflix.com/in/title/70143860'
+      
+    },
+    {
+        imgsrc:'https://m.media-amazon.com/images/M/MV5BMWVhNTgwMWEtN2U4Yi00NDhhLWIxOWQtNDc2Y2Y1MTFmM2U1XkEyXkFqcGdeQXVyMTEwMTcyMzkz._V1_FMjpg_UX1000_.jpg',
+        title:'A Netflix Orignal series',
+        sname:'My first two love',
+        link:'https://www.netflix.com/in/title/81026700'
+      
+    },
+    {
+        imgsrc:'https://m.media-amazon.com/images/M/MV5BMWVhNTgwMWEtN2U4Yi00NDhhLWIxOWQtNDc2Y2Y1MTFmM2U1XkEyXkFqcGdeQXVyMTEwMTcyMzkz._V1_FMjpg_UX1000_.jpg',
+        title:'A Netflix Orignal series',
+        sname:'My first two love',
+        link:'https://www.netflix.com/in/title/81026700'
+      
+    },
+    ];
+    
+    export default Sdata;
+    
--- src/index.js
+++ src/index.js
@@ -1,17 +1,86 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDom from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Card from './Card';
+import Sdata from './Sdata';
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDom.render(
+  <>
+  <h1>Top 5 Netflix web series </h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <Card
+  imgsrc={Sdata[0].imgsrc}
+  title={Sdata[0].title}
+  sname={Sdata[0].sname}
+  link={Sdata[0].link}
+  />
+
+<Card
+  imgsrc={Sdata[1].imgsrc}
+  title={Sdata[1].title}
+  sname={Sdata[1].sname}
+  link={Sdata[1].link}
+  />
+
+<Card
+  imgsrc={Sdata[2].imgsrc}
+  title={Sdata[2].title}
+  sname={Sdata[2].sname}
+  link={Sdata[2].link}
+  />
+
+<Card
+  imgsrc={Sdata[3].imgsrc}
+  title={Sdata[3].title}
+  sname={Sdata[3].sname}
+  link={Sdata[3].link}
+  />
+
+<Card
+  imgsrc={Sdata[4].imgsrc}
+  title={Sdata[4].title}
+  sname={Sdata[4].sname}
+  link={Sdata[4].link}
+  />
+
+<Card
+  imgsrc={Sdata[5].imgsrc}
+  title={Sdata[5].title}
+  sname={Sdata[5].sname}
+  link={Sdata[5].link}
+  />
+
+<Card
+  imgsrc={Sdata[6].imgsrc}
+  title={Sdata[6].title}
+  sname={Sdata[6].sname}
+  link={Sdata[6].link}
+  />
+
+<Card
+  imgsrc={Sdata[7].imgsrc}
+  title={Sdata[7].title}
+  sname={Sdata[7].sname}
+  link={Sdata[7].link}
+  />
+
+<Card
+  imgsrc={Sdata[8].imgsrc}
+  title={Sdata[8].title}
+  sname={Sdata[8].sname}
+  link={Sdata[8].link}
+  />
+
+
+
+
+
+
+  </>,document.getElementById("root"));
+
